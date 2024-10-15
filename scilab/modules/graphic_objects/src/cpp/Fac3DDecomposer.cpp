GerHobbelt/scilab

commit bd066ba9627981e43a773ad809b61c546e423c88

Add surface cdata bounds and color range
These features existed for the Fec entity and were cruelly missing to the Fac3d entity. The new proposed properties are `cdata_bounds` (equivalent to `z_bounds` in Fec) and `color_range` (same name as Fec feature). Here is the example in the updated surface_properties help page:
```
clf;
subplot(2,2,1);
nc = 128;
gcf().color_map = [coolwarm(nc); spectral(nc)];
[X,Y]=meshgrid(-1:0.1:1,-1:0.1:1);
surf(X,Y,X.^2-Y.^2,"facecolor","interp","edgecolor")
gce().color_range = [nc+1 2*nc];

subplot(2,2,2);
surf(X,Y,-X.^2-Y.^2,"facecolor","interp");
gce().color_range = [1 nc];

subplot(2,2,3);
surf(X,Y,X.^2-Y.^2,"facecolor","interp");
gce().color_range = [nc+1 2*nc];
gce().cdata_bounds = [0 1];

subplot(2,2,4);
surf(X,Y,-X.^2-Y.^2,"facecolor","interp");
gce().color_range = [1 nc];
gce().cdata_bounds = [-1 -0.5];
```
![scshot.svg](/uploads/b3851f5b78b01071c6beeb9f5d3921dd/scshot.svg)

Co-authored-by: Vincent COUVERT <vincent.couvert@3ds.com>
See https://gitlab.com/scilab/scilab/-/merge_requests/474

scilab/modules/graphic_objects/src/cpp/Fac3DDecomposer.cpp
@@ -112,10 +112,12 @@ void Fac3DDecomposer::fillTextureCoordinates(int id, float* buffer, int bufferLe
     int* pparentFigure = &parentFigure;
     int parent = 0;
     int* pparent = &parent;
+    int* colorRange = NULL;
 
-    double* colors = NULL;
+    double* cData = NULL;
     double* colormap = NULL;
     double* z = NULL;
+    double* cDataBounds = NULL;
 
     double color = 0.;
 
@@ -126,6 +128,7 @@ void Fac3DDecomposer::fillTextureCoordinates(int id, float* buffer, int bufferLe
     int numColors = 0;
     int* piNumColors = &numColors;
 
+    int colorsNumber = 0.;
     int colormapSize = 0;
     int* piColormapSize = &colormapSize;
 
@@ -142,7 +145,7 @@ void Fac3DDecomposer::fillTextureCoordinates(int id, float* buffer, int bufferLe
     getGraphicObjectProperty(id, __GO_DATA_MODEL_NUM_GONS__, jni_int, (void**) &piNumGons);
 
     getGraphicObjectProperty(id, __GO_DATA_MODEL_NUM_COLORS__, jni_int, (void**) &piNumColors);
-    getGraphicObjectProperty(id, __GO_DATA_MODEL_COLORS__, jni_double_vector, (void**) &colors);
+    getGraphicObjectProperty(id, __GO_DATA_MODEL_COLORS__, jni_double_vector, (void**) &cData);
 
 
     parent = getParentObject(id);
@@ -170,7 +173,19 @@ void Fac3DDecomposer::fillTextureCoordinates(int id, float* buffer, int bufferLe
     }
 
     getGraphicObjectProperty(parentFigure, __GO_COLORMAP__, jni_double_vector, (void**) &colormap);
+    getGraphicObjectProperty(id, __GO_COLOR_RANGE__, jni_int_vector, (void**) &colorRange);
     getGraphicObjectProperty(parentFigure, __GO_COLORMAP_SIZE__, jni_int, (void**) &piColormapSize);
+
+    if (colorRange[0] != 0 || colorRange[1] != 0)
+    {
+        colorsNumber = 1 + colorRange[1] - colorRange[0];
+    }
+    else
+    {
+        colorsNumber = colormapSize;
+    }
+
+    releaseGraphicObjectProperty(__GO_COLOR_RANGE__, colorRange, jni_int_vector, 0);
 
     if (numColors == numGons * numVerticesPerGon)
     {
@@ -181,12 +196,15 @@ void Fac3DDecomposer::fillTextureCoordinates(int id, float* buffer, int bufferLe
         perVertex = 0;
     }
 
-
     getGraphicObjectProperty(id, __GO_DATA_MODEL_Z__, jni_int, (void**) &z);
+    getGraphicObjectProperty(id, __GO_CDATA_BOUNDS__, jni_double_vector, (void**) &cDataBounds);
 
     if (colorFlag == 1)
     {
-        fillNormalizedZColorsTextureCoordinates(buffer, bufferLength, colormap, colormapSize, z, numGons, numVerticesPerGon);
+        /*
+         * color is proportional to data.z
+        */
+        fillNormalizedZColorsTextureCoordinates(buffer, bufferLength, colormap, colorsNumber, z, numGons, numVerticesPerGon, cDataBounds);
     }
     else if (colorFlag > 1 && numColors == 0)
     {
@@ -207,15 +225,18 @@ void Fac3DDecomposer::fillTextureCoordinates(int id, float* buffer, int bufferLe
     }
     else
     {
-        fillDataColorsTextureCoordinates(buffer, bufferLength, colormap, colormapSize,
-                                         colors, colorFlag, perVertex, dataMapping, numGons, numVerticesPerGon);
+       /*
+        * color is proportional to data.color
+       */
+        fillDataColorsTextureCoordinates(buffer, bufferLength, colormap, colorsNumber,
+                                         cData, colorFlag, perVertex, dataMapping, numGons, numVerticesPerGon, cDataBounds);
     }
 
     releaseGraphicObjectProperty(__GO_COLORMAP__, colormap, jni_double_vector, colormapSize);
 }
 
 void Fac3DDecomposer::fillNormalizedZColorsTextureCoordinates(float* buffer, int bufferLength, double* colormap, int colormapSize,
-        double* z, int numGons, int numVerticesPerGon)
+        double* z, int numGons, int numVerticesPerGon, double *cDataBounds)
 {
     double zavg = 0.;
     double zMin = 0.;
@@ -227,8 +248,17 @@ void Fac3DDecomposer::fillNormalizedZColorsTextureCoordinates(float* buffer, int
     int i = 0;
     int j = 0;
     int bufferOffset = 0;
+    
 
-    computeMinMaxValues(z, numGons * numVerticesPerGon, numGons, numVerticesPerGon, ALL_VALUES, &zMin, &zMax);
+    if ((cDataBounds[0] != 0.0 || cDataBounds[1] != 0.0) && (DecompositionUtils::isValid(cDataBounds[0]) && DecompositionUtils::isValid(cDataBounds[1])) && (cDataBounds[0] != cDataBounds[1]))
+    {
+        zMin = cDataBounds[0];
+        zMax = cDataBounds[1];
+    }
+    else
+    {
+        computeMinMaxValues(z, numGons * numVerticesPerGon, numGons, numVerticesPerGon, ALL_VALUES, &zMin, &zMax);
+    }
 
     minDoubleValue = DecompositionUtils::getMinDoubleValue();
 
@@ -277,10 +307,10 @@ void Fac3DDecomposer::fillConstantColorsTextureCoordinates(float* buffer, int bu
 }
 
 void Fac3DDecomposer::fillDataColorsTextureCoordinates(float* buffer, int bufferLength, double* colormap, int colormapSize,
-        double* colors, int colorFlag, int perVertex, int dataMapping, int numGons, int numVerticesPerGon)
+        double* cData, int colorFlag, int perVertex, int cDataMapping, int numGons, int numVerticesPerGon, double *cDataBounds)
 {
-    double colMin = 0.;
-    double colRange = 0.;
+    double cDataMin = 0.;
+    double cDataRange = 0.;
     double color = 0.;
     double colorTextureOffset = 0.;
     double index = 0.;
@@ -300,12 +330,21 @@ void Fac3DDecomposer::fillDataColorsTextureCoordinates(float* buffer, int buffer
         numColors = numGons;
     }
 
-    colorComputer = Fac3DColorComputer(colors, numColors, colorFlag, dataMapping, numGons, numVerticesPerGon);
+    colorComputer = Fac3DColorComputer(cData, numColors, colorFlag, cDataMapping, numGons, numVerticesPerGon);
 
-    /* 0: colors are scaled */
-    if (dataMapping == 0)
+
+    /* 0: color data is mapped/scaled to colormap subset (cdata_mapping == "scaled") */
+    if (cDataMapping == 0)
     {
-        colorComputer.getColorRangeValue(&colMin, &colRange);
+        if ((cDataBounds[0] != 0.0 || cDataBounds[1] != 0.0) && (DecompositionUtils::isValid(cDataBounds[0]) && DecompositionUtils::isValid(cDataBounds[1])) && (cDataBounds[0] != cDataBounds[1]))
+        {
+            cDataMin = cDataBounds[0];
+            cDataRange = cDataBounds[1]-cDataBounds[0];
+        }
+        else
+        {
+            colorComputer.getColorRangeValue(&cDataMin, &cDataRange);
+        }
     }
 
     /*
@@ -321,14 +360,14 @@ void Fac3DDecomposer::fillDataColorsTextureCoordinates(float* buffer, int buffer
         {
             color = colorComputer.getOutputFacetColor(i, j);
 
-            if (dataMapping == 1)
+            if (cDataMapping == 1)
             {
                 color = DecompositionUtils::getAbsoluteValue(color);
                 index = ColorComputer::getClampedDirectIndex(color - 1.0 , colormapSize);
             }
-            else if (dataMapping == 0)
+            else if (cDataMapping == 0)
             {
-                index = ColorComputer::getIndex(color, colMin, colRange, COLOR_OFFSET, 0, colormapSize - 1);
+                index = ColorComputer::getIndex(color, cDataMin, cDataRange, COLOR_OFFSET, 0, colormapSize - 1);
             }
 
             /* The offset corresponding to the black and white colors must added to the index and the colormap size. */
@@ -438,7 +477,7 @@ int Fac3DDecomposer::fillIndices(int id, int* buffer, int bufferLength, int logM
     double yc = 0.;
     double zc = 0.;
 
-    double* colors = NULL;
+    double* cData = NULL;
 
     int numVerticesPerGon = 0;
     int* piNumVerticesPerGon = &numVerticesPerGon;
@@ -463,7 +502,7 @@ int Fac3DDecomposer::fillIndices(int id, int* buffer, int bufferLength, int logM
     getGraphicObjectProperty(id, __GO_DATA_MODEL_NUM_GONS__, jni_int, (void**) &piNumGons);
 
     getGraphicObjectProperty(id, __GO_DATA_MODEL_NUM_COLORS__, jni_int, (void**) &piNumColors);
-    getGraphicObjectProperty(id, __GO_DATA_MODEL_COLORS__, jni_double_vector, (void**) &colors);
+    getGraphicObjectProperty(id, __GO_DATA_MODEL_COLORS__, jni_double_vector, (void**) &cData);
 
     getGraphicObjectProperty(id, __GO_COLOR_FLAG__, jni_int, (void**) &piColorFlag);
 
@@ -475,7 +514,7 @@ int Fac3DDecomposer::fillIndices(int id, int* buffer, int bufferLength, int logM
         return 0;
     }
 
-    colorComputer = Fac3DColorComputer(colors, numColors, colorFlag, dataMapping, numGons, numVerticesPerGon);
+    colorComputer = Fac3DColorComputer(cData, numColors, colorFlag, dataMapping, numGons, numVerticesPerGon);
 
     getGraphicObjectProperty(id, __GO_DATA_MODEL_X__, jni_double_vector, (void**) &x);
     getGraphicObjectProperty(id, __GO_DATA_MODEL_Y__, jni_double_vector, (void**) &y);

scilab/modules/graphic_objects/includes/Fac3DDecomposer.hxx
@@ -58,9 +58,10 @@ private :
      * @param[in] the z coordinate array.
      * @param[in] the number of N-gons.
      * @param[in] the number of vertices per N-gon.
+     * @param[in] a pointer to the color bounds array
      */
     static void fillNormalizedZColorsTextureCoordinates(float* buffer, int bufferLength, double* colormap, int colormapSize,
-            double* z, int numGons, int numVerticesPerGon);
+            double* z, int numGons, int numVerticesPerGon, double *colorbounds);
 
     /**
      * Fills a buffer with a single color.
@@ -89,9 +90,10 @@ private :
      * @param[in] a flag specifying whether color values are linearly mapped to the color map (0) or a direct mapping is used (1).
      * @param[in] the number of N-gons.
      * @param[in] the number of vertices per N-gon.
+     * @param[in] a pointer to the color bounds array
      */
     static void fillDataColorsTextureCoordinates(float* buffer, int bufferLength, double* colormap, int colormapSize,
-            double* colors, int colorFlag, int perVertex, int dataMapping, int numGons, int numVerticesPerGon);
+            double* colors, int colorFlag, int perVertex, int dataMapping, int numGons, int numVerticesPerGon, double *colorbounds);
 
     /**
      * Computes the average value of an N-gon's vertex values.

scilab/modules/graphic_objects/includes/graphicObjectProperties.h
@@ -244,186 +244,187 @@
 #define __GO_OUTSIDE_COLOR__ 222
 #define __GO_COLOR_RANGE__ 223
 #define __GO_DATA_MAPPING__ 224
-#define __GO_FONT_ANGLE__ 225
-#define __GO_AUTO_POSITION__ 226
-#define __GO_CORNERS__ 227
-#define __GO_AUTO_ROTATION__ 228
-#define __GO_LINKS__ 229
-#define __GO_LINKS_COUNT__ 230
-#define __GO_LEGEND_LOCATION__ 231
-#define __GO_MARKS_COUNT__ 232
-#define __GO_LINE_WIDTH__ 233
-#define __GO_CLOSED__ 234
-#define __GO_ARROW_SIZE_FACTOR__ 235
-#define __GO_POLYLINE_STYLE__ 236
-#define __GO_INTERP_COLOR_VECTOR__ 237
-#define __GO_INTERP_COLOR_VECTOR_SET__ 238
-#define __GO_INTERP_COLOR_MODE__ 239
-#define __GO_X_SHIFT__ 240
-#define __GO_Y_SHIFT__ 241
-#define __GO_Z_SHIFT__ 242
-#define __GO_BAR_WIDTH__ 243
-#define __GO_SURFACE_MODE__ 244
-#define __GO_COLOR_MODE__ 245
-#define __GO_COLOR_FLAG__ 246
-#define __GO_ALIGNMENT__ 247
-#define __GO_BOX__ 248
-#define __GO_TEXT_BOX__ 249
-#define __GO_TEXT_BOX_MODE__ 250
-#define __GO_AUTO_DIMENSIONING__ 251
-#define __GO_FORMATTED_TEXT__ 252
-#define __GO_TEXT_ARRAY_DIMENSIONS__ 253
-#define __GO_TEXT_STRINGS__ 254
-#define __GO_BASE__ 255
-#define __GO_BASE_X__ 256
-#define __GO_BASE_Y__ 257
-#define __GO_BASE_Z__ 258
-#define __GO_DIRECTION__ 259
-#define __GO_DIRECTION_X__ 260
-#define __GO_DIRECTION_Y__ 261
-#define __GO_DIRECTION_Z__ 262
-#define __GO_ARROW_SIZE__ 263
-#define __GO_SEGS_COLORS__ 264
-#define __GO_COLORED__ 265
-#define __GO_ARROWS__ 266
-#define __GO_NUMBER_ARROWS__ 267
-#define __GO_CHAMP_DIMENSIONS__ 268
-#define __GO_BOUNDING_BOX__ 269
-#define __GO_MAX_LENGTH__ 270
-#define __GO_MAX_USABLE_LENGTH__ 271
-#define __GO_HIDDEN_COLOR__ 272
-#define __GO_STYLE__ 273
-#define __GO_UI_CHECKBOX__ 274
-#define __GO_UI_EDIT__ 275
-#define __GO_UI_SPINNER__ 276
-#define __GO_UI_FRAME__ 277
-#define __GO_UI_FRAME_SCROLLABLE__ 278
-#define __GO_UI_IMAGE__ 279
-#define __GO_UI_LISTBOX__ 280
-#define __GO_UI_POPUPMENU__ 281
-#define __GO_UI_PUSHBUTTON__ 282
-#define __GO_UI_RADIOBUTTON__ 283
-#define __GO_UI_SLIDER__ 284
-#define __GO_UI_TABLE__ 285
-#define __GO_UI_TEXT__ 286
-#define __GO_UI_LAYER__ 287
-#define __GO_UI_BACKGROUNDCOLOR__ 288
-#define __GO_UI_ENABLE__ 289
-#define __GO_UI_FONTANGLE__ 290
-#define __GO_UI_FONTNAME__ 291
-#define __GO_UI_FONTSIZE__ 292
-#define __GO_UI_FONTUNITS__ 293
-#define __GO_UI_FONTWEIGHT__ 294
-#define __GO_UI_FOREGROUNDCOLOR__ 295
-#define __GO_UI_HORIZONTALALIGNMENT__ 296
-#define __GO_UI_LISTBOXTOP__ 297
-#define __GO_UI_LISTBOXTOP_SIZE__ 298
-#define __GO_UI_MAX__ 299
-#define __GO_UI_MIN__ 300
-#define __GO_UI_RELIEF__ 301
-#define __GO_UI_STRING__ 302
-#define __GO_UI_TAB_STRING__ 303
-#define __GO_UI_STRING_SIZE__ 304
-#define __GO_UI_STRING_COLNB__ 305
-#define __GO_UI_TOOLTIPSTRING__ 306
-#define __GO_UI_TOOLTIPSTRING_SIZE__ 307
-#define __GO_UI_SLIDERSTEP__ 308
-#define __GO_UI_SNAPTOTICKS__ 309
-#define __GO_UI_UNITS__ 310
-#define __GO_UI_VALUE__ 311
-#define __GO_UI_TAB_VALUE__ 312
-#define __GO_UI_VALUE_SIZE__ 313
-#define __GO_UI_VERTICALALIGNMENT__ 314
-#define __GO_UIPARENTMENU__ 315
-#define __GO_UICHILDMENU__ 316
-#define __GO_UICHECKEDMENU__ 317
-#define __GO_UI_CHECKED__ 318
-#define __GO_UI_LABEL__ 319
-#define __GO_UI_MNEMONIC__ 320
-#define __GO_UI_ACCELERATOR__ 321
-#define __GO_UI_SEPARATOR__ 322
-#define __GO_UI_ICON__ 323
-#define __GO_CONSOLE__ 324
-#define __GO_SHOWHIDDENHANDLES__ 325
-#define __GO_SHOWHIDDENPROPERTIES__ 326
-#define __GO_USEDEPRECATEDLF__ 327
-#define __GO_WAITBAR__ 328
-#define __GO_PROGRESSIONBAR__ 329
-#define __GO_UI_MESSAGE__ 330
-#define __GO_UI_MESSAGE_SIZE__ 331
-#define __GO_DATATIP__ 332
-#define __GO_DATATIP_DATA__ 333
-#define __GO_DATATIP_ORIENTATION__ 334
-#define __GO_DATATIP_DISPLAY_COMPONENTS__ 335
-#define __GO_DATATIP_AUTOORIENTATION__ 336
-#define __GO_DATATIP_INTERP_MODE__ 337
-#define __GO_DATATIP_DISPLAY_FNC__ 338
-#define __GO_DATATIP_BOX_MODE__ 339
-#define __GO_DATATIP_LABEL_MODE__ 340
-#define __GO_DATATIP_MARK__ 341
-#define __GO_DATATIP_DETACHED_MODE__ 342
-#define __GO_DATATIP_DETACHED_POSITION__ 343
-#define __GO_AMBIENTCOLOR__ 344
-#define __GO_DIFFUSECOLOR__ 345
-#define __GO_SPECULARCOLOR__ 346
-#define __GO_COLOR_MATERIAL__ 347
-#define __GO_MATERIAL_SHININESS__ 348
-#define __GO_LIGHT__ 349
-#define __GO_LIGHT_TYPE__ 350
-#define __GO_DATATIPS__ 351
-#define __GO_DATATIPS_COUNT__ 352
-#define __GO_DATATIP_INDEXES__ 353
-#define __GO_DATATIP_DISPLAY_MODE__ 354
-#define __GO_DATA_MODEL_DISPLAY_FUNCTION__ 355
-#define __GO_DATA_MODEL_DISPLAY_FUNCTION_SIZE__ 356
-#define __GO_RESIZE__ 357
-#define __GO_TOOLBAR__ 358
-#define __GO_TOOLBAR_VISIBLE__ 359
-#define __GO_MENUBAR__ 360
-#define __GO_MENUBAR_VISIBLE__ 361
-#define __GO_INFOBAR_VISIBLE__ 362
-#define __GO_DOCKABLE__ 363
-#define __GO_LAYOUT__ 364
-#define __GO_LAYOUT_SET__ 365
-#define __GO_UI_TAB__ 366
-#define __GO_UI_GRIDBAG_GRID__ 367
-#define __GO_UI_GRIDBAG_WEIGHT__ 368
-#define __GO_UI_GRIDBAG_FILL__ 369
-#define __GO_UI_GRIDBAG_ANCHOR__ 370
-#define __GO_UI_GRIDBAG_PADDING__ 371
-#define __GO_UI_GRIDBAG_PREFERREDSIZE__ 372
-#define __GO_UI_GRID_GRID__ 373
-#define __GO_UI_GRID_PADDING__ 374
-#define __GO_UI_BORDER_POSITION__ 375
-#define __GO_UI_BORDER_PREFERREDSIZE__ 376
-#define __GO_GRID_OPT_GRID__ 377
-#define __GO_GRID_OPT_PADDING__ 378
-#define __GO_BORDER_OPT_PADDING__ 379
-#define __GO_UI_FRAME_BORDER__ 380
-#define __GO_UI_FRAME_BORDER_COLOR__ 381
-#define __GO_UI_FRAME_BORDER_HIGHLIGHT_IN__ 382
-#define __GO_UI_FRAME_BORDER_HIGHLIGHT_OUT__ 383
-#define __GO_UI_FRAME_BORDER_IN_BORDER__ 384
-#define __GO_UI_FRAME_BORDER_JUSTIFICATION__ 385
-#define __GO_UI_FRAME_BORDER_OUT_BORDER__ 386
-#define __GO_UI_FRAME_BORDER_ROUNDED__ 387
-#define __GO_UI_FRAME_BORDER_SHADOW_IN__ 388
-#define __GO_UI_FRAME_BORDER_SHADOW_OUT__ 389
-#define __GO_UI_FRAME_BORDER_TITLE__ 390
-#define __GO_UI_FRAME_BORDER_POSITION__ 391
-#define __GO_UI_FRAME_BORDER_STYLE__ 392
-#define __GO_UI_GROUP_NAME__ 393
-#define __GO_UI_TITLE_POSITION__ 394
-#define __GO_UI_TITLE_SCROLL__ 395
-#define __GO_UI_FRAME_BORDER_TYPE__ 396
-#define __GO_UI_SCROLLABLE__ 397
-#define __GO_COLOR_SET__ 398
-#define __GO_MARK_SIZES__ 399
-#define __GO_NUM_MARK_SIZES__ 400
-#define __GO_MARK_FOREGROUNDS__ 401
-#define __GO_NUM_MARK_FOREGROUNDS__ 402
-#define __GO_MARK_BACKGROUNDS__ 403
-#define __GO_NUM_MARK_BACKGROUNDS__ 404
-#define __GO_UI_FOCUS__ 405
+#define __GO_CDATA_BOUNDS__ 225
+#define __GO_FONT_ANGLE__ 226
+#define __GO_AUTO_POSITION__ 227
+#define __GO_CORNERS__ 228
+#define __GO_AUTO_ROTATION__ 229
+#define __GO_LINKS__ 230
+#define __GO_LINKS_COUNT__ 231
+#define __GO_LEGEND_LOCATION__ 232
+#define __GO_MARKS_COUNT__ 233
+#define __GO_LINE_WIDTH__ 234
+#define __GO_CLOSED__ 235
+#define __GO_ARROW_SIZE_FACTOR__ 236
+#define __GO_POLYLINE_STYLE__ 237
+#define __GO_INTERP_COLOR_VECTOR__ 238
+#define __GO_INTERP_COLOR_VECTOR_SET__ 239
+#define __GO_INTERP_COLOR_MODE__ 240
+#define __GO_X_SHIFT__ 241
+#define __GO_Y_SHIFT__ 242
+#define __GO_Z_SHIFT__ 243
+#define __GO_BAR_WIDTH__ 244
+#define __GO_SURFACE_MODE__ 245
+#define __GO_COLOR_MODE__ 246
+#define __GO_COLOR_FLAG__ 247
+#define __GO_ALIGNMENT__ 248
+#define __GO_BOX__ 249
+#define __GO_TEXT_BOX__ 250
+#define __GO_TEXT_BOX_MODE__ 251
+#define __GO_AUTO_DIMENSIONING__ 252
+#define __GO_FORMATTED_TEXT__ 253
+#define __GO_TEXT_ARRAY_DIMENSIONS__ 254
+#define __GO_TEXT_STRINGS__ 255
+#define __GO_BASE__ 256
+#define __GO_BASE_X__ 257
+#define __GO_BASE_Y__ 258
+#define __GO_BASE_Z__ 259
+#define __GO_DIRECTION__ 260
+#define __GO_DIRECTION_X__ 261
+#define __GO_DIRECTION_Y__ 262
+#define __GO_DIRECTION_Z__ 263
+#define __GO_ARROW_SIZE__ 264
+#define __GO_SEGS_COLORS__ 265
+#define __GO_COLORED__ 266
+#define __GO_ARROWS__ 267
+#define __GO_NUMBER_ARROWS__ 268
+#define __GO_CHAMP_DIMENSIONS__ 269
+#define __GO_BOUNDING_BOX__ 270
+#define __GO_MAX_LENGTH__ 271
+#define __GO_MAX_USABLE_LENGTH__ 272
+#define __GO_HIDDEN_COLOR__ 273
+#define __GO_STYLE__ 274
+#define __GO_UI_CHECKBOX__ 275
+#define __GO_UI_EDIT__ 276
+#define __GO_UI_SPINNER__ 277
+#define __GO_UI_FRAME__ 278
+#define __GO_UI_FRAME_SCROLLABLE__ 279
+#define __GO_UI_IMAGE__ 280
+#define __GO_UI_LISTBOX__ 281
+#define __GO_UI_POPUPMENU__ 282
+#define __GO_UI_PUSHBUTTON__ 283
+#define __GO_UI_RADIOBUTTON__ 284
+#define __GO_UI_SLIDER__ 285
+#define __GO_UI_TABLE__ 286
+#define __GO_UI_TEXT__ 287
+#define __GO_UI_LAYER__ 288
+#define __GO_UI_BACKGROUNDCOLOR__ 289
+#define __GO_UI_ENABLE__ 290
+#define __GO_UI_FONTANGLE__ 291
+#define __GO_UI_FONTNAME__ 292
+#define __GO_UI_FONTSIZE__ 293
+#define __GO_UI_FONTUNITS__ 294
+#define __GO_UI_FONTWEIGHT__ 295
+#define __GO_UI_FOREGROUNDCOLOR__ 296
+#define __GO_UI_HORIZONTALALIGNMENT__ 297
+#define __GO_UI_LISTBOXTOP__ 298
+#define __GO_UI_LISTBOXTOP_SIZE__ 299
+#define __GO_UI_MAX__ 300
+#define __GO_UI_MIN__ 301
+#define __GO_UI_RELIEF__ 302
+#define __GO_UI_STRING__ 303
+#define __GO_UI_TAB_STRING__ 304
+#define __GO_UI_STRING_SIZE__ 305
+#define __GO_UI_STRING_COLNB__ 306
+#define __GO_UI_TOOLTIPSTRING__ 307
+#define __GO_UI_TOOLTIPSTRING_SIZE__ 308
+#define __GO_UI_SLIDERSTEP__ 309
+#define __GO_UI_SNAPTOTICKS__ 310
+#define __GO_UI_UNITS__ 311
+#define __GO_UI_VALUE__ 312
+#define __GO_UI_TAB_VALUE__ 313
+#define __GO_UI_VALUE_SIZE__ 314
+#define __GO_UI_VERTICALALIGNMENT__ 315
+#define __GO_UIPARENTMENU__ 316
+#define __GO_UICHILDMENU__ 317
+#define __GO_UICHECKEDMENU__ 318
+#define __GO_UI_CHECKED__ 319
+#define __GO_UI_LABEL__ 320
+#define __GO_UI_MNEMONIC__ 321
+#define __GO_UI_ACCELERATOR__ 322
+#define __GO_UI_SEPARATOR__ 323
+#define __GO_UI_ICON__ 324
+#define __GO_CONSOLE__ 325
+#define __GO_SHOWHIDDENHANDLES__ 326
+#define __GO_SHOWHIDDENPROPERTIES__ 327
+#define __GO_USEDEPRECATEDLF__ 328
+#define __GO_WAITBAR__ 329
+#define __GO_PROGRESSIONBAR__ 330
+#define __GO_UI_MESSAGE__ 331
+#define __GO_UI_MESSAGE_SIZE__ 332
+#define __GO_DATATIP__ 333
+#define __GO_DATATIP_DATA__ 334
+#define __GO_DATATIP_ORIENTATION__ 335
+#define __GO_DATATIP_DISPLAY_COMPONENTS__ 336
+#define __GO_DATATIP_AUTOORIENTATION__ 337
+#define __GO_DATATIP_INTERP_MODE__ 338
+#define __GO_DATATIP_DISPLAY_FNC__ 339
+#define __GO_DATATIP_BOX_MODE__ 340
+#define __GO_DATATIP_LABEL_MODE__ 341
+#define __GO_DATATIP_MARK__ 342
+#define __GO_DATATIP_DETACHED_MODE__ 343
+#define __GO_DATATIP_DETACHED_POSITION__ 344
+#define __GO_AMBIENTCOLOR__ 345
+#define __GO_DIFFUSECOLOR__ 346
+#define __GO_SPECULARCOLOR__ 347
+#define __GO_COLOR_MATERIAL__ 348
+#define __GO_MATERIAL_SHININESS__ 349
+#define __GO_LIGHT__ 350
+#define __GO_LIGHT_TYPE__ 351
+#define __GO_DATATIPS__ 352
+#define __GO_DATATIPS_COUNT__ 353
+#define __GO_DATATIP_INDEXES__ 354
+#define __GO_DATATIP_DISPLAY_MODE__ 355
+#define __GO_DATA_MODEL_DISPLAY_FUNCTION__ 356
+#define __GO_DATA_MODEL_DISPLAY_FUNCTION_SIZE__ 357
+#define __GO_RESIZE__ 358
+#define __GO_TOOLBAR__ 359
+#define __GO_TOOLBAR_VISIBLE__ 360
+#define __GO_MENUBAR__ 361
+#define __GO_MENUBAR_VISIBLE__ 362
+#define __GO_INFOBAR_VISIBLE__ 363
+#define __GO_DOCKABLE__ 364
+#define __GO_LAYOUT__ 365
+#define __GO_LAYOUT_SET__ 366
+#define __GO_UI_TAB__ 367
+#define __GO_UI_GRIDBAG_GRID__ 368
+#define __GO_UI_GRIDBAG_WEIGHT__ 369
+#define __GO_UI_GRIDBAG_FILL__ 370
+#define __GO_UI_GRIDBAG_ANCHOR__ 371
+#define __GO_UI_GRIDBAG_PADDING__ 372
+#define __GO_UI_GRIDBAG_PREFERREDSIZE__ 373
+#define __GO_UI_GRID_GRID__ 374
+#define __GO_UI_GRID_PADDING__ 375
+#define __GO_UI_BORDER_POSITION__ 376
+#define __GO_UI_BORDER_PREFERREDSIZE__ 377
+#define __GO_GRID_OPT_GRID__ 378
+#define __GO_GRID_OPT_PADDING__ 379
+#define __GO_BORDER_OPT_PADDING__ 380
+#define __GO_UI_FRAME_BORDER__ 381
+#define __GO_UI_FRAME_BORDER_COLOR__ 382
+#define __GO_UI_FRAME_BORDER_HIGHLIGHT_IN__ 383
+#define __GO_UI_FRAME_BORDER_HIGHLIGHT_OUT__ 384
+#define __GO_UI_FRAME_BORDER_IN_BORDER__ 385
+#define __GO_UI_FRAME_BORDER_JUSTIFICATION__ 386
+#define __GO_UI_FRAME_BORDER_OUT_BORDER__ 387
+#define __GO_UI_FRAME_BORDER_ROUNDED__ 388
+#define __GO_UI_FRAME_BORDER_SHADOW_IN__ 389
+#define __GO_UI_FRAME_BORDER_SHADOW_OUT__ 390
+#define __GO_UI_FRAME_BORDER_TITLE__ 391
+#define __GO_UI_FRAME_BORDER_POSITION__ 392
+#define __GO_UI_FRAME_BORDER_STYLE__ 393
+#define __GO_UI_GROUP_NAME__ 394
+#define __GO_UI_TITLE_POSITION__ 395
+#define __GO_UI_TITLE_SCROLL__ 396
+#define __GO_UI_FRAME_BORDER_TYPE__ 397
+#define __GO_UI_SCROLLABLE__ 398
+#define __GO_COLOR_SET__ 399
+#define __GO_MARK_SIZES__ 400
+#define __GO_NUM_MARK_SIZES__ 401
+#define __GO_MARK_FOREGROUNDS__ 402
+#define __GO_NUM_MARK_FOREGROUNDS__ 403
+#define __GO_MARK_BACKGROUNDS__ 404
+#define __GO_NUM_MARK_BACKGROUNDS__ 405
+#define __GO_UI_FOCUS__ 406
 
 #endif /* !__GRAPHIC_OBJECT_PROPERTIES_H__ */

commit db559c8a840fa4f64deaef32af90702595bda84a

Fix #17314: plot3d color output is fixed (was broken after bd066ba9)
The problem was only affecting cdata_mapping="direct" case.

Closes #17314
See https://gitlab.com/scilab/scilab/-/merge_requests/1101

scilab/modules/graphic_objects/src/cpp/Fac3DDecomposer.cpp
@@ -371,7 +371,7 @@ void Fac3DDecomposer::fillDataColorsTextureCoordinates(float* buffer, int buffer
             }
 
             /* The offset corresponding to the black and white colors must added to the index and the colormap size. */
-            buffer[bufferOffset++] = (float) ((index + colorTextureOffset + 2.0) / (double) (colormapSize + 2));
+            buffer[bufferOffset++] = (float) ((index + colorTextureOffset + 1.0) / (double) (colormapSize + 2));
             buffer[bufferOffset++] = 0;
             buffer[bufferOffset++] = 0;
             buffer[bufferOffset++] = 1.0;