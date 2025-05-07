GerHobbelt/scilab

commit 6199f90e499311e96f9262f7be025b352d5d3d0a

fix #17412: when h was an Axes handle, legend(h, ...) used gca() instead
Fix test on handle type after cb5077f4a521

Co-authored-by: Vincent COUVERT <vincent.couvert@3ds.com>
Closes #17412
See https://gitlab.com/scilab/scilab/-/merge_requests/1326

scilab/modules/graphics/macros/legend.sci
@@ -75,8 +75,8 @@ function varargout=legend(varargin)
     if type(varargin(k0))==9 then //a handle that could be an Axes, Agreg. or Polyline handle.
         tmpH=varargin(k0)
         if tmpH.type=="Axes" | tmpH.type=="Compound" then
-            H = getvalidchildren(A)($:-1:1);
-        else
+            H = getvalidchildren(tmpH)($:-1:1);
+        elseif tmpH.type=="Polyline"
             H = tmpH;
         end
         k0 = k0+1;

scilab/modules/graphics/tests/nonreg_tests/issue_17412.tst
@@ -1,0 +1,28 @@
+// =============================================================================
+// Scilab ( https://www.scilab.org/ ) - This file is part of Scilab
+// Copyright (C) 2025 - UTC - Stéphane MOTTELET
+//
+//  This file is distributed under the same license as the Scilab package.
+// =============================================================================
+//
+// <-- TEST WITH GRAPHIC -->
+// <-- NO CHECK REF -->
+//
+// <-- Non-regression test for issue 17412 -->
+//
+// <-- GitLab URL -->
+// https://gitlab.com/scilab/scilab/-/issues/17412
+//
+// <-- Short Description -->
+// when h was an Axes handle legend(h, ...) used gca() instead 
+
+clf();
+subplot(1,2,1);
+h1 = gca();
+plot(1:10,sin(1:10),'r');
+subplot(1,2,2);
+h2 = gca();
+plot(1:10,sin(1:10));
+leg1 = legend(h1,"red sine");
+found_leg = findobj(h1,"type","Legend");
+assert_checkequal(found_leg,leg1);

commit be63751b6328e401cdad267197999e3b166ff4ac

bug 9269 NR test was failing after 6199f90e
Fix support and deprecate undocumented feature (Compound entities as first argument are supported, although never said as such in the documentation).

See https://gitlab.com/scilab/scilab/-/merge_requests/1339

scilab/modules/graphics/macros/legend.sci
@@ -72,12 +72,21 @@ function varargout=legend(varargin)
         end
     end
 
-    if type(varargin(k0))==9 then //a handle that could be an Axes, Agreg. or Polyline handle.
-        tmpH=varargin(k0)
-        if tmpH.type=="Axes" | tmpH.type=="Compound" then
+    if type(varargin(k0))==9 then //a handle that could be an Axes or array of Polyline handle.
+        tmpH=varargin(k0);
+        if size(tmpH,"*") == 1 && tmpH.type=="Axes"
             H = getvalidchildren(tmpH)($:-1:1);
-        elseif tmpH.type=="Polyline"
+        elseif tmpH.type=="Compound"
+            for k=1:size(tmpH,"*")
+                H = [H; getvalidchildren(tmpH(k))($:-1:1)];
+            end
+            msg = msprintf(_("%s: using a Compound or a Compound array as first argument is obsolete. "),"legend");
+            msg = [msg, msprintf(_("This feature will be permanently removed in Scilab %s"), "2026.0.0")]
+            warning(msg);
+        elseif  tmpH.type == "Polyline"
             H = tmpH;
+        else
+            error(msprintf(_("%s: Wrong type for input argument #%d: single Axes handle or array of Polyline handles expected\n"), "legend", k0))     
         end
         k0 = k0+1;
     elseif type(varargin(k0)) <> 10